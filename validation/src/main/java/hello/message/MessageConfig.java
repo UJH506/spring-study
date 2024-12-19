53b8232cba4bb972dbac8b614764b9d1774b92d2
UJH506 <djawlgp456@gmail.com>
2024-12-19 18:38:13 +0900

    검증

---
+++ validation/src/main/java/hello/message/MessageConfig.java
@@ -0,0 +1,21 @@
+package hello.message;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+        // 설정파일명 지정 (messages.properties파일, errors.properties파일을 읽겠다는 의미)
+        messageSource.setBasenames("messages", "errors");
+        messageSource.setDefaultEncoding("utf-8");
+
+        return messageSource;
+    }
+}
---
+++ validation/src/main/java/hello/message/web/item/basic/BasicItemController.java
@@ -0,0 +1,127 @@
+package hello.message.web.item.basic;
+
+import hello.message.domain.item.Item;
+import hello.message.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /* 상품상세보기 */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /* 상품 추가 폼 */
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    /* 상품 추가 액션
+    // @RequestParam 으로 하나씩 받기
+    @PostMapping("/add")
+    public String addItemV1(@RequestParam("itemName") String itemName,
+                            @RequestParam("price") int price,
+                            @RequestParam("quantity") Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    // @ModelAttribute("모델이름지정") 로 한번에 받기
+    // model.addAttribute("item", item); 자동 추가
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // @ModelAttribute 로 이름생략
+    // model.addAttribute("item", item); 자동 추가
+    // 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+    // 매개변수에 Model model도 생략
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // @ModelAttribute 자체를 생략
+    // model.addAttribute(item) 자동 추가
+    // 매개변수에 Model model도 생략
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // PRG - Post/Redirect/Get
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+    */
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /* 상품수정폼 */
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    
+    /* 상품 수정 액션 */
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable("itemId") Long itemId, Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /*
+    * 테스트용 데이터 추가
+    * */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
