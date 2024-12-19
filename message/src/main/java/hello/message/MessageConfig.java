79b129b982387e4d9cd3e167a2c7b0615413d59b
UJH506 <djawlgp456@gmail.com>
2024-12-19 18:09:50 +0900

    메시지, 국제화

--- message/src/main/java/hello/message/MessageConfig.java
+++ message/src/main/java/hello/message/MessageConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
-public class ItemConfig {
+public class MessageConfig {
 
     @Bean
     public MessageSource messageSource() {
--- message/src/test/java/hello/message/MessageApplicationTests.java
+++ message/src/test/java/hello/message/MessageApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ItemServiceApplicationTests {
+class MessageApplicationTests {
 
 	@Test
 	void contextLoads() {
