4dcb1f19bcbe88ad48ceb9dc54715637f4653686
UJH506 <djawlgp456@gmail.com>
2024-12-24 11:58:13 +0900

    어노테이션 생성으로 공통작업 최적화

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,11 +1,15 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.interceptor.LogInterceptor;
 import hello.login.web.interceptor.LoginCheckInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -25,5 +29,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/", "/members/add", "/login", "/logout",
                         "/css/**", "/*.ico", "/error"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,66 +26,20 @@ public class HomeController {
 
     /*
     @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-    */
-
-    /*
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
-        if (memberId == null) {
-            return "home";
-        }
-
-        // 로그인
-        Member loginMember = memberRepository.findById(memberId);
-        if(loginMember == null) {
-            return "home";
-        }
-
-        model.addAttribute("member", loginMember);
-        return "loginHome";
-    }
-
-    @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model) {
-
-        // 세션 관리자에 저장된 회원정보 조회
-        Member member = (Member)sessionManager.getSession(request);
-
-        if (member == null) {
-            return "home";
-        }
-
-        model.addAttribute("member", member);
-        return "loginHome";
-    }
-
-    @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model) {
-        HttpSession session = request.getSession(false);
-
-        // 세션이 없으면 home
-        if (session == null) {
-            return "home";
-        }
-
-        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
-
-        // 세션에 데이터가 없으면 home
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        // 세션에 회원 데이터가 없으면 false
         if (loginMember == null) {
             return "home";
         }
 
-        // 세션이 유지되면 로그인홈으로 이동
+        // 세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
     */
 
     @GetMapping("/")
-    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
         // 세션에 회원 데이터가 없으면 false
         if (loginMember == null) {
             return "home";
